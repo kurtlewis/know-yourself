7a56d8d737aaf439bc69620f68f14eaec7092460
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-02-10 16:20:15 -0500

    Add delete and insertMultiple dao methods

--- app/src/main/java/com/kurtjlewis/knowyourself/db/dao/FeelingDao.java
+++ app/src/main/java/com/kurtjlewis/knowyourself/db/dao/FeelingDao.java
@@ -7,6 +7,7 @@ package com.kurtjlewis.knowyourself.db.dao;
  */
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
@@ -27,4 +28,10 @@ public interface FeelingDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertFeeling(FeelingEntity feeling);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertFeelings(List<FeelingEntity> feelings);
+
+    @Delete
+    void deleteFeeling(FeelingEntity feeling);
 }
