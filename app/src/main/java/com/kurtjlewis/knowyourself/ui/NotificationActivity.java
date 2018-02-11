6d17232e294002d7e8a2edc1fed64b4b7634f03a
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-02-11 09:35:58 -0500

    Schedule notifications

--- app/src/main/java/com/kurtjlewis/knowyourself/ui/NotificationActivity.java
+++ app/src/main/java/com/kurtjlewis/knowyourself/ui/NotificationActivity.java
@@ -1,12 +1,24 @@
 package com.kurtjlewis.knowyourself.ui;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.kurtjlewis.knowyourself.R;
+import com.kurtjlewis.knowyourself.service.NotificationPublisher;
 
-public class NotificationActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class NotificationActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +26,11 @@ public class NotificationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notification);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // Set Listener for switches
+        ((Switch)findViewById(R.id.morningSwitch)).setOnCheckedChangeListener(this);
+        ((Switch)findViewById(R.id.afternoonSwitch)).setOnCheckedChangeListener(this);
+        ((Switch)findViewById(R.id.eveningSwitch)).setOnCheckedChangeListener(this);
+        ((Switch)findViewById(R.id.nightSwitch)).setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -25,5 +42,41 @@ public class NotificationActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton v, boolean isChecked) {
+        Calendar calendar = Calendar.getInstance();
+        int requestCode = 0;
+        switch (v.getId()){
+            case R.id.morningSwitch:
+                // TODO: For debugging purposes this triggers a notificiation in 1 minute
+                calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE) + 1);
+                //calendar.set(Calendar.HOUR_OF_DAY, 1);
+                //calendar.set(Calendar.MINUTE, 14);
+                requestCode = 1;
+                break;
+            case R.id.afternoonSwitch:
+                calendar.set(Calendar.HOUR_OF_DAY, 13);
+                requestCode = 2;
+                break;
+            case R.id.eveningSwitch:
+                calendar.set(Calendar.HOUR_OF_DAY, 18);
+                requestCode = 3;
+                break;
+            case R.id.nightSwitch:
+                calendar.set(Calendar.HOUR_OF_DAY, 22);
+                requestCode = 4;
+                break;
+        }
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, NotificationPublisher.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, requestCode, intent, 0);
+        if (isChecked) {
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 24*60*60*1000, pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+        }
+
     }
 }
