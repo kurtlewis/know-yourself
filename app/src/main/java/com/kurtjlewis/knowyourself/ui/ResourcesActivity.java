6025ed7b672e9924cfbec9f46a503254ca8f7fee
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-02-11 10:23:47 -0500

    Create Resources activity

---
+++ app/src/main/java/com/kurtjlewis/knowyourself/ui/ResourcesActivity.java
@@ -0,0 +1,16 @@
+package com.kurtjlewis.knowyourself.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.kurtjlewis.knowyourself.R;
+
+public class ResourcesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources);
+    }
+
+}
